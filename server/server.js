8d79b93a5d0e353af4347b355642700b89196fbb
Koushikhazra <hkoushik058@gmail.com>
2025-11-10 15:04:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ import topicRoutes from './routes/topics.js';
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? process.env.CLIENT_URL 
+    : 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
