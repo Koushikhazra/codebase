9074881405f415bbddb6553e2bb44680d68c8c3c
Koushikhazra <hkoushik058@gmail.com>
2025-11-29 14:02:10 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -37,8 +37,8 @@ export function Layout({ children }) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-8">
-              <h1 className="text-xl font-bold text-gray-900 dark:text-white">
-                    
+              <h1 className="  font-bold text-gray-900 dark:text-white flex items-center whitespace-nowrap">
+                <Code className="w-5 h-4 mr-1 text-blue-600" />
                 Codebase
               </h1>
               
