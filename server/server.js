28a45dd2435fabb5b157be633e28cbe8009eb171
Koushikhazra <hkoushik058@gmail.com>
2025-12-05 21:16:44 +0530

    Fix CORS origin URL for Netlify app

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://cdebase.netlify.app',
+    'https://codebse.netlify.app',
     'https://codebase-dusky.vercel.app/'
   ]
 }));
